b7a4e6030c2121e816aad4229395e9a48cce3f94
GalGog <galynna00@gmail.com>
2019-06-23 11:28:14 +0100

    get select options from candidates

--- src/components/SelectionBox.js
+++ src/components/SelectionBox.js
@@ -4,28 +4,29 @@ import {newCandidates} from '../data';
 class SelectionBox extends React.Component {
 
     getSkills() {
-        let skills = [];
+        let uniqueSkills = [];
+        // eslint-disable-next-line array-callback-return
         newCandidates.filter(function (candidate) {
+            // eslint-disable-next-line array-callback-return
             candidate.skills.filter(function (skill) {
-                if (!skills.includes(skill)) {
-                    skills.push(skill);
+                if (!uniqueSkills.includes(skill)) {
+                    uniqueSkills.push(skill);
                 }
             });
         });
-        return skills
+        return uniqueSkills
     }
 
-    renderSelect() {
-        this.getSkills();
+    static renderSelect(options, className, onChange) {
 
         const MakeItem = function (X) {
             return <option key={X}>{X}</option>;
         };
         return <select
-            className="list"
-            onChange={this.props.handleOnChange}
+            className = {className}
+            onChange = {onChange}
         >
-            {this.getSkills().map(MakeItem)}
+            {options.map(MakeItem)}
         </select>;
     };
 
@@ -33,7 +34,7 @@ class SelectionBox extends React.Component {
         return (
             <div className="filter">
                 <label>Filter by Skills
-                    {this.renderSelect()}
+                    {SelectionBox.renderSelect(this.getSkills(), 'list', this.props.handleOnChange)}
                 </label>
             </div>
         );
